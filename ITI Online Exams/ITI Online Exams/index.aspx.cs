16c8287a46d983c4025028cba1c037b7c5108d6f
mrwan hossam <mrwan hossam@MRWAN_HOSSAM>
2018-02-21 15:22:52 +0200

    add test index wep page

---
+++ ITI Online Exams/ITI Online Exams/index.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ITI_Online_Exams
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
